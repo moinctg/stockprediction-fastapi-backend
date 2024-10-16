083899c134b653397245ca2c86213bac43d61f16
Md Moinuddin Kamal <cpimoinuddin@gmail.com>
2024-10-17 00:35:09 +0600

    update code

--- stock_prediction_fastapi/app/api/endpoints/stock_data.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.models.stock  import StockData
-
-router = APIRouter()
-
-@router.post("/add-stock-data/")
-async def add_stock_data(stock_data: StockData):
-    # Logic to add stock data
-    pass
--- stock_prediction_fastapi/app/db/mongo_DB.py
+++
@@ -1,7 +0,0 @@
-# MongoDB configuration
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_DETAILS = "mongodb+srv://cpimoinuddin:moin123456@cluster0.wh6jr.mongodb.net/Stock-Db?retryWrites=true&w=majority&appName=Cluster0"  # Change this for your MongoDB connection
-client = AsyncIOMotorClient(MONGO_DETAILS)
-database = client.stock_Db
-stock_collection = database.get_collection("stocks")
